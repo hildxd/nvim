b519623b9aa559efe1511538f51afd89d8a6f447
hildxd <hildxd@outlook.com>
2024-06-28 11:35:48 +0800

    first commit

---
+++ lua/community.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
+}

b58e220066595e8974d784dae214cf1890baa9ed
hildxd <hildxd@outlook.com>
2024-07-12 17:52:52 +0800

    update

--- lua/community.lua
+++ lua/community.lua
@@ -3,11 +3,10 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.completion.copilot-cmp" },
-  { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
   { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
 }
---
+++ lua/plugins/flash.lua
@@ -0,0 +1,56 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          x = {
+            ["<CR>"] = {
+              function() require("flash").jump() end,
+              desc = "Flash",
+            },
+            ["R"] = {
+              function() require("flash").treesitter_search() end,
+              desc = "Treesitter Search",
+            },
+            ["<S-CR>"] = {
+              function() require("flash").treesitter() end,
+              desc = "Flash Treesitter",
+            },
+          },
+          o = {
+            ["r"] = {
+              function() require("flash").remote() end,
+              desc = "Remote Flash",
+            },
+            ["R"] = {
+              function() require("flash").treesitter_search() end,
+              desc = "Treesitter Search",
+            },
+            ["<CR>"] = {
+              function() require("flash").jump() end,
+              desc = "Flash",
+            },
+            ["<S-CR>"] = {
+              function() require("flash").treesitter() end,
+              desc = "Flash Treesitter",
+            },
+          },
+          n = {
+            ["<CR>"] = {
+              function() require("flash").jump() end,
+              desc = "Flash",
+            },
+            ["<S-CR>"] = {
+              function() require("flash").treesitter() end,
+              desc = "Flash Treesitter",
+            },
+          },
+        },
+      },
+    },
+  },
+  opts = {},
+}

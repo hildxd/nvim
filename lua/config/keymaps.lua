c72a9e1b06e78a2a780ae19a485df57384ec81bf
hildxd <hildxd@outlook.com>
2024-07-28 21:05:17 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,1 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.del({ "n", "i", "v" }, "<leader>ca")
+vim.keymap.del("n", "<leader>ca")
